4b799556a46067bca07b870f6e28417900cc383a
alexandriaware <ali.mercer92@gmail.com>
2017-10-26 14:39:25 -0400

    got rid of table

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -12,11 +13,15 @@ namespace Bookshelf.Models
         public int ID { get; set; }
         [MinLength(2), MaxLength(50)] //Now our Title needs a minimum of two characters but not more than fifty.
         public string Title { get; set; }
+        [Display(Name ="Date Published")]
         public DateTime PublishedDate { get; set; }
         [MaxLength(500)]
         public string Description { get; set; }
         public string ISBN { get; set; }
 
+        [ForeignKey("Category"), Display(Name = "Genre")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
 
     }
 }
